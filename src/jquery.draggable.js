f3dc210cae974bb83d6b504141061390e25a5ad4
Vincenzo Ampolo <vincenzo@draios.com>
2014-11-24 23:17:01 -0800

    Fix for initial draggable state

--- src/jquery.draggable.js
+++ src/jquery.draggable.js
@@ -416,7 +416,7 @@
             return this.options.ignore_dragging(event);
         }
 
-        var res = true;
+        var res = false;
         var shouldIgnore = $(event.target).is(this.options.ignore_dragging.join(', '));
 
         if (this.options.handle) {
